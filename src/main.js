358dff419c3483eebde68706c4d2cf23389dd5fb
lk <2995201114@qq.com>
2023-01-16 09:36:50 +0800

    feat：新增图标

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import {Edit, CopyDocument, Delete} from '@element-plus/icons-vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
@@ -8,6 +9,7 @@ import router from './router'
 
 import './assets/main.css'
 
+const icons = { Edit, CopyDocument, Delete };
 const app = createApp(App)
 
 app.use(createPinia())
@@ -16,4 +18,7 @@ app.use(router)
 // 引入ElementPlus
 app.use(ElementPlus)
 
+Object.entries(icons).forEach((comp) => {
+    app.component(comp[0], comp[1]);
+});
 app.mount('#app')
