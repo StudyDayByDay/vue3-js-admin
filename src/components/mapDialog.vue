18a5ffb96400ec97a2879034d55e71d46fa382d8
lk <2995201114@qq.com>
2023-02-08 14:35:31 +0800

    fix：修改映射组件

--- src/components/mapDialog.vue
+++ src/components/mapDialog.vue
@@ -3,38 +3,104 @@
         <div class="content">
             <div class="content-item">
                 <span class="title">实体：</span>
-                <span>{{ '疾病综合征' }}</span>
+                <span>{{ entity.label }}</span>
             </div>
             <div class="content-item">
                 <span class="title">映射实体：</span>
+                <el-tag
+                    v-for="tag in dynamicTags"
+                    :key="tag"
+                    class="mx-1"
+                    closable
+                    :disable-transitions="false"
+                    @close="tagClose(tag)"
+                >
+                    {{ tag }}
+                </el-tag>
+                <el-input
+                    v-if="inputVisible"
+                    ref="InputRef"
+                    v-model="inputValue"
+                    class="input"
+                    size="small"
+                    @keyup.enter="handleInputConfirm"
+                    @blur="handleInputConfirm"
+                />
+                <el-button v-else class="button-new-tag ml-1" size="small" @click="showInput">
+                    + 新实体
+                </el-button>
             </div>
         </div>
         <template #footer>
             <span class="dialog-footer">
-                <el-button type="primary">提交</el-button>
-                <el-button>取消</el-button>
+                <el-button type="primary" @click="handleDialog('commit')">提交</el-button>
+                <el-button @click="handleDialog('cancel')">取消</el-button>
             </span>
         </template>
     </el-dialog>
 </template>
 
 <script setup>
-import { computed } from 'vue';
+import { computed, nextTick, ref } from 'vue';
+import { ElInput } from 'element-plus'
 
 const props = defineProps({
     modelValue: {
         type: Boolean,
         required: true
+    },
+    entity: {
+        type: Object,
+        default: () => {
+            return {
+                label: '',
+                id: ''
+            }
+        }
     }
 });
 
-const emit = defineEmits(['update:modelValue']);
+const emit = defineEmits(['update:modelValue', 'commit']);
+
+const dynamicTags = ref([]);
+const inputVisible = ref(false);
+const InputRef = ref(null);
+const inputValue = ref('');
 
 const mapVisible = computed(() => {
     return props.modelValue;
 });
+
+const tagClose = (tag) => {
+  dynamicTags.value.splice(dynamicTags.value.indexOf(tag), 1);
+}
+
+const showInput = () => {
+  inputVisible.value = true;
+  nextTick(() => {
+    InputRef.value.input.focus();
+  })
+}
+
+const handleInputConfirm = () => {
+  if (inputValue.value) {
+    dynamicTags.value.push(inputValue.value);
+  }
+  inputVisible.value = false;
+  inputValue.value = '';
+}
+
 const handleClose = () => {
-  emit('update:modelValue', false);
+    emit('update:modelValue', false);
+}
+const handleDialog = (e) => {
+    if (e === 'commit') {
+        emit('commit', {
+            pid: props.entity.id,
+            mapText: dynamicTags.value,
+        });
+    }
+    handleClose();
 }
 
 </script>
@@ -42,9 +108,19 @@ const handleClose = () => {
 <style lang="scss" scoped>
 .content {
     &-item {
+        margin-bottom: 10px;
         .title {
             font-weight: bold;
+            margin-right: 10px;
+        }
+        .input {
+            width: 100px;
         }
     }
+}
+
+:deep(.el-tag) {
+    margin: 0 5px 5px 0;
+    user-select: none;
 }
 </style>
